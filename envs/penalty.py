003404a31de60b5ee9afbfcfa7969708ae625109
acrucha <mcaa@cin.ufpe.br>
2024-09-23 19:53:01 -0300

    fix(VSSPenaltyEnv): fixing vss penalty environment

--- envs/penalty.py
+++ envs/penalty.py
@@ -6,10 +6,9 @@ from typing import Dict, List
 import gymnasium as gym
 import numpy as np
 from rsoccer_gym.Entities import Frame, Robot, Ball
-from rsoccer_gym.vss.vss_gym_base import VSSBaseEnv, RSimVSS
+from rsoccer_gym.vss.vss_gym_base import RSimVSS
+from rsoccer_gym.vss.env_vss import VSSEnv
 from rsoccer_gym.Utils import KDTree
-
-from torchrl.data import CompositeSpec, DiscreteTensorSpec, UnboundedContinuousTensorSpec
 
 N_ROBOTS_BLUE = 1
 N_ROBOTS_YELLOW = 1
@@ -21,13 +20,13 @@ REWARD_NORM_BOUND = 5.0
 TO_CENTIMETERS = 100
 OBSERVATIONS_SIZE = 18
 OUTPUT_SIZE = 2
-ROBOT_HALF_AXIS = 37.5
+ROBOT_HALF_AXIS = 37.5 / 1000
 WHEEL_RADIUS = 24.0
 MAX_V_WHEEL = 50.0
 MAX_V_LINEAR = 1.2
 MAX_W_ANGULAR = 1.5
 
-class VSSPenaltyEnv(VSSBaseEnv):
+class VSSPenaltyEnv(VSSEnv):
     """This environment controls a single robot to go to a target position using PID control. 
 
         Description:
@@ -71,21 +70,26 @@ class VSSPenaltyEnv(VSSBaseEnv):
     """
 
     def __init__(self, render_mode=None, repeat_action=1, max_steps=500):
-        super().__init__(field_type=0, n_robots_blue=N_ROBOTS_BLUE, n_robots_yellow=N_ROBOTS_YELLOW,
-                         time_step=0.025, render_mode=render_mode)
-        
+        super().__init__(render_mode=render_mode)
+
+        self.n_robots_yellow = N_ROBOTS_YELLOW
+        self.n_robots_blue = N_ROBOTS_BLUE
+
         self.rsim = RSimVSS(
             field_type=0,
-            n_robots_blue=N_ROBOTS_BLUE,
-            n_robots_yellow=N_ROBOTS_YELLOW,
+            n_robots_blue=self.n_robots_blue,
+            n_robots_yellow=self.n_robots_yellow,
             time_step_ms=int(self.time_step * 1000),
         )
 
-        self.action_space = gym.spaces.Box(low=-MAX_W_ANGULAR, high=MAX_W_ANGULAR,
-                                           shape=(OUTPUT_SIZE, ), dtype=np.float32)
+        self.action_space = gym.spaces.Box(
+            low=-1, high=1, shape=(2,), dtype=np.float32
+        )
+        
         self.observation_space = gym.spaces.Box(low=-self.NORM_BOUNDS,
                                                 high=self.NORM_BOUNDS,
                                                 shape=(OBSERVATIONS_SIZE, ), dtype=np.float32)
+        
 
         # Initialize Class Atributes
         self.previous_ball_potential = None
@@ -100,10 +104,6 @@ class VSSPenaltyEnv(VSSBaseEnv):
                 OrnsteinUhlenbeckAction(self.action_space, dt=self.time_step)
             )
         
-        self.action_spec = CompositeSpec({
-            "action": UnboundedContinuousTensorSpec(1)
-        })
-
         self.max_steps = max_steps
         self.repeat_action = repeat_action
 
@@ -116,6 +116,7 @@ class VSSPenaltyEnv(VSSBaseEnv):
         print('Environment initialized')
 
     def reset(self, *, seed=None, options=None):
+        obs, _ = super().reset(seed=seed, options=options)
         self.actions = None
         self.reward_info = None
         self.previous_ball_potential = None
@@ -124,7 +125,44 @@ class VSSPenaltyEnv(VSSBaseEnv):
         for ou in self.ou_actions:
             ou.reset()
 
-        return super().reset(seed=seed, options=options)
+        return obs, {}
+    
+    def _get_initial_positions_frame(self):
+        '''Returns the position of each robot and ball for the initial frame'''
+        field_half_length = self.field.length / 2
+        field_half_width = self.field.width / 2
+
+        goal = self.field.goal_width / 2
+        
+        print(f"field_half_length = {field_half_length}; field_half_width = {field_half_width}")
+
+        def gk_x(): return random.uniform(field_half_length - self.robot_half_axis, field_half_length)
+
+        def gk_y(): return random.uniform(-goal, goal)
+
+        def x(): return random.uniform(field_half_length/4, field_half_length/3)
+
+        def y(): return random.uniform(-field_half_width/3, field_half_width/3)
+
+        def theta(): return random.uniform(0, 360)
+
+        pos_frame: Frame = Frame()
+
+        pos_frame.ball = Ball(x=field_half_length/2, y=0)
+
+        places = KDTree()
+        places.insert((pos_frame.ball.x, pos_frame.ball.y))
+        
+        # pos = (field_half_width - ROBOT_HALF_AXIS - 100, 0)
+        pos = (gk_x(), gk_y())
+        places.insert(pos)
+        pos_frame.robots_yellow[0] = Robot(x=pos[0], y=pos[1], theta=theta())
+        
+        pos = (x(), y())
+        places.insert(pos)
+        pos_frame.robots_blue[0] = Robot(x=pos[0], y=pos[1], theta=theta())
+
+        return pos_frame
     
     def step(self, action):
         self.cur_action = action
@@ -189,27 +227,27 @@ class VSSPenaltyEnv(VSSBaseEnv):
         return np.array(observation, dtype=np.float32)
 
 
-    def _actions_to_v_wheels(self, linear_velocity, angular_velocity):
+    def _actions_to_v_wheels(self, left_wheel_speed, right_wheel_speed):
+        left_wheel_speed *= self.max_v
+        right_wheel_speed *= self.max_v
 
-        left_motor_speed, right_motor_speed = 0, 0
-
-        linear_velocity = np.clip(linear_velocity, -MAX_V_LINEAR, MAX_V_LINEAR)
-        
-        left_motor_speed = linear_velocity - (angular_velocity * WHEEL_RADIUS)
-        right_motor_speed = linear_velocity + (angular_velocity * WHEEL_RADIUS)
-
-        left_motor_speed, right_motor_speed = np.clip(
-            (left_motor_speed, right_motor_speed), -MAX_V_WHEEL, MAX_V_WHEEL
+        left_wheel_speed, right_wheel_speed = np.clip(
+            (left_wheel_speed, right_wheel_speed), -self.max_v, self.max_v
         )
 
         # Deadzone
-        if -self.v_wheel_deadzone < left_motor_speed < self.v_wheel_deadzone:
-            left_motor_speed = 0
+        if -self.v_wheel_deadzone < left_wheel_speed < self.v_wheel_deadzone:
+            left_wheel_speed = 0
 
-        if -self.v_wheel_deadzone < right_motor_speed < self.v_wheel_deadzone:
-            right_motor_speed = 0
+        if -self.v_wheel_deadzone < right_wheel_speed < self.v_wheel_deadzone:
+            right_wheel_speed = 0
 
-        return left_motor_speed , right_motor_speed
+        # Convert to rad/s
+        left_wheel_speed /= self.field.rbt_wheel_radius
+        right_wheel_speed /= self.field.rbt_wheel_radius
+
+        return left_wheel_speed, right_wheel_speed
+
 
     def _get_commands(self, actions):
         commands = []
@@ -219,25 +257,24 @@ class VSSPenaltyEnv(VSSBaseEnv):
         v_wheel0, v_wheel1 = self._actions_to_v_wheels(actions[0], actions[1])
         commands.append(Robot(yellow=False, id=0, v_wheel0=v_wheel0, v_wheel1=v_wheel1))
 
-        print(f"action rad/s: v_wheel0 = {v_wheel0}; v_wheel1 = {v_wheel1}")
+        # print(f"action rad/s: v_wheel0 = {v_wheel0}; v_wheel1 = {v_wheel1}")
 
         # Send random commands to the GK
-        for i in range(0, self.n_robots_yellow):
-            actions = self.ou_actions[i].sample()
-            self.actions[i] = actions
-            v_wheel0, v_wheel1 = self._actions_to_v_wheels(actions[0], actions[1])
-            commands.append(Robot(yellow=True, id=i, v_wheel0=v_wheel0,
-                                  v_wheel1=v_wheel1))
+        actions = self.ou_actions[0].sample()
+        v_wheel0, v_wheel1 = self._actions_to_v_wheels(actions[0], actions[1])
+        commands.append(Robot(yellow=True, id=0, v_wheel0=v_wheel0,
+                                v_wheel1=v_wheel1))
 
         return commands
 
     def _calculate_reward_and_done(self):
         reward = 0
         goal = False
-        w_move = 0.2
-        w_ball_grad = 0.6
-        w_energy = 2e-4
-        w_gk = 0.2
+        w_move = 0.6
+        w_ball_grad = 3
+        w_energy = 0.0184
+        w_goal = 10
+        w_gk = 0.1
         if self.reward_info is None:
             self.reward_info = {
                 'goal_score': 0, 
@@ -251,13 +288,12 @@ class VSSPenaltyEnv(VSSBaseEnv):
         # Check if goal ocurred
         if self.frame.ball.x > (self.field.length / 2):
             self.reward_info['goal_score'] += 1
-            reward = 1000
+            reward = 1 * w_goal
             goal = True
         elif self.steps == self.max_steps - 1:
-            reward = -1000
+            reward = -1 * w_goal
             goal = False
         else:
-
             if self.last_frame is not None:
                 # Calculate ball potential
                 grad_ball_potential = self.__ball_grad()
@@ -274,75 +310,32 @@ class VSSPenaltyEnv(VSSBaseEnv):
                     w_gk * gk_reaches_ball
 
                 self.reward_info['move'] += w_move * move_reward
-                self.reward_info['ball_grad'] += w_ball_grad \
-                    * grad_ball_potential
-                self.reward_info['energy'] += w_energy \
-                    * energy_penalty
+                self.reward_info['ball_grad'] += w_ball_grad * grad_ball_potential
+                self.reward_info['energy'] += w_energy * energy_penalty
                 self.reward_info['gk'] += w_gk * gk_reaches_ball
 
         self.reward_info['reward_total'] += reward
 
         return reward, goal
 
-    def _get_initial_positions_frame(self):
-        '''Returns the position of each robot and ball for the initial frame'''
-        field_half_length = self.field.length / 2
-        field_half_width = self.field.width / 2
-
-        def x(): return random.uniform(-field_half_width + 0.1,
-                                       field_half_width - 0.1)
-
-        def y(): return random.uniform(-field_half_length/2 + 0.1, field_half_length/2 - 0.1)
-
-        def theta(): return random.uniform(0, 360)
-
-        pos_frame: Frame = Frame()
-
-        pos_frame.ball = Ball(x=field_half_length/2, y=0)
-
-        min_dist = 0.1
-
-        places = KDTree()
-        places.insert((pos_frame.ball.x, pos_frame.ball.y))
-        
-        for i in range(self.n_robots_blue):
-            pos = (field_half_length/3, y())
-            while places.get_nearest(pos)[1] < min_dist:
-                pos = (x(), y())
-
-            places.insert(pos)
-            pos_frame.robots_blue[i] = Robot(x=pos[0], y=pos[1], theta=theta())
-
-        for i in range(self.n_robots_yellow):
-            pos = (field_half_length, y())
-            while places.get_nearest(pos)[1] < min_dist:
-                pos = (x(), y())
-
-            places.insert(pos)
-            pos_frame.robots_yellow[i] = Robot(x=pos[0], y=pos[1], theta=theta())
-
-        return pos_frame
-
-
     def __move_reward(self):
-        '''Calculate Move to ball reward
+        """Calculate Move to ball reward
 
         Cosine between the robot vel vector and the vector robot -> ball.
         This indicates rather the robot is moving towards the ball or not.
-        '''
+        """
 
         ball = np.array([self.frame.ball.x, self.frame.ball.y])
-        robot = np.array([self.frame.robots_blue[0].x,
-                          self.frame.robots_blue[0].y])
-        robot_vel = np.array([self.frame.robots_blue[0].v_x,
-                              self.frame.robots_blue[0].v_y])
+        robot = np.array([self.frame.robots_blue[0].x, self.frame.robots_blue[0].y])
+        robot_vel = np.array(
+            [self.frame.robots_blue[0].v_x, self.frame.robots_blue[0].v_y]
+        )
         robot_ball = ball - robot
-        robot_ball = robot_ball/np.linalg.norm(robot_ball)
+        robot_ball = robot_ball / np.linalg.norm(robot_ball)
 
         move_reward = np.dot(robot_ball, robot_vel)
 
-        move_reward = np.clip(move_reward / 0.4, -5.0, 5.0)
-        return move_reward
+        return move_reward / 1.2
 
     def __energy_penalty(self):
         '''Calculates the energy penalty'''
@@ -350,16 +343,15 @@ class VSSPenaltyEnv(VSSBaseEnv):
         en_penalty_1 = abs(self.sent_commands[0].v_wheel0)
         en_penalty_2 = abs(self.sent_commands[0].v_wheel1)
         energy_penalty = - (en_penalty_1 + en_penalty_2)
-        return energy_penalty
+        return energy_penalty / 92
     
     def __ball_grad(self):
-        '''Calculate ball potential gradient
+        """Calculate ball potential gradient
         Difference of potential of the ball in time_step seconds.
-        '''
+        """
         # Calculate ball potential
         length_cm = self.field.length * 100
-        half_lenght = (self.field.length / 2.0)\
-            + self.field.goal_depth
+        half_lenght = (self.field.length / 2.0) + self.field.goal_depth
 
         # distance to defence
         dx_d = (half_lenght + self.frame.ball.x) * 100
@@ -367,21 +359,21 @@ class VSSPenaltyEnv(VSSBaseEnv):
         dx_a = (half_lenght - self.frame.ball.x) * 100
         dy = (self.frame.ball.y) * 100
 
-        dist_1 = -math.sqrt(dx_a ** 2 + 2 * dy ** 2)
-        dist_2 = math.sqrt(dx_d ** 2 + 2 * dy ** 2)
+        dist_1 = -np.sqrt(dx_a**2 + 2 * dy**2)
+        dist_2 = np.sqrt(dx_d**2 + 2 * dy**2)
         ball_potential = ((dist_1 + dist_2) / length_cm - 1) / 2
 
         grad_ball_potential = 0
         # Calculate ball potential gradient
         # = actual_potential - previous_potential
         if self.previous_ball_potential is not None:
-            diff = ball_potential - self.previous_ball_potential
-            grad_ball_potential = np.clip(diff * 3 / self.time_step,
-                                          -5.0, 5.0)
+            grad_ball_potential = (
+                ball_potential - self.previous_ball_potential
+            ) / self.time_step
 
         self.previous_ball_potential = ball_potential
 
-        return grad_ball_potential
+        return grad_ball_potential / 0.8
         
     def __gk_reaches_ball(self):
         '''Calculates the reward when the gk reaches the ball'''
